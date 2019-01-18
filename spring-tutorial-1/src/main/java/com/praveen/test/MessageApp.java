951ce888b885949d4ab4a5870bb9dfbe134b8f17
praveen <praveensinghps1379@gmail.com>
2019-01-19 04:54:22 +0530

    retrieved MessageService bean from context and printed hello msg

--- spring-tutorial-1/src/main/java/com/praveen/test/MessageApp.java
+++ spring-tutorial-1/src/main/java/com/praveen/test/MessageApp.java
@@ -1,12 +1,17 @@
 package com.praveen.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 import com.praveen.servce.MessageService;
 
 public class MessageApp {
 
 	public static void main(String[] args) {
-		MessageService messageService=new MessageService();
+		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("beans.xml");
+		MessageService messageService=(MessageService)applicationContext.getBean("msg");
 		messageService.sayHello();
+		((ClassPathXmlApplicationContext)applicationContext).close();
 	}
 
 }
